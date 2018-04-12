d47b166dbc3202e5c0bec12efbc73c3f3535623c
Derni Ageng <derni.ageng@ui.ac.id>
2018-04-12 16:20:08 +0700

    Pemindahan IDE

--- jhotel/TestTryCatch.java
+++
@@ -1,8 +0,0 @@
-public class TestTryCatch{  
-  public static void main(String args[]){  
-   try{  
-      int data=50/0;  
-   }catch(ArithmeticException e){System.out.println(e);}  
-   System.out.println("Next line");  
-}  
-}  
